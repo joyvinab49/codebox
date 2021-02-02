84df6017d9cc5d2615e9c44d932ea3a0fb778932
joyvinab49 <zhixinwolail@163.com>
2021-02-03 00:02:05 +0800

    ts

---
+++ src/components/changeTypes.vue
@@ -0,0 +1,31 @@
+<template>
+  <select v-model="seleType" @change="msgNew" ref="typeNames">
+    <option v-for="(type, index) in msg" :key="index">
+      {{ type.typeName }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "changeTypes",
+  data() {
+    return {
+      seleType: "",
+    };
+  },
+  props: {
+    msg: {
+      type: Array,
+    },
+  },
+  methods: {
+    msgNew() {
+      this.$emit("input", this.seleType);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
